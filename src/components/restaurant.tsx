b142a5d3b05b566ecd3a745267b3b9604b8930f7
MoltenPL85 <bugaev.maxim.85@gmail.com>
2021-02-13 23:01:00 +0500

    Restaurant page

--- src/components/restaurant.tsx
+++ src/components/restaurant.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface IRestaurantProps {
   id: string;
@@ -8,20 +9,23 @@ interface IRestaurantProps {
 }
 
 export const Restaurant: React.FC<IRestaurantProps> = ({
+  id,
   coverImg,
   name,
   categoryName,
 }) => {
   return (
-    <div className="flex flex-col">
-      <div
-        style={{ backgroundImage: `url(${coverImg})` }}
-        className="py-28 bg-gray-500 bg-cover bg-center mb-3"
-      ></div>
-      <h3 className="text-xl font-medium">{name}</h3>
-      <span className="border-t mt-3 py-2 text-xs opacity-50 border-gray-400">
-        {categoryName}
-      </span>
-    </div>
+    <Link to={`/restaurants/${id}`}>
+      <div className="flex flex-col">
+        <div
+          style={{ backgroundImage: `url(${coverImg})` }}
+          className="py-28 bg-gray-500 bg-cover bg-center mb-3"
+        ></div>
+        <h3 className="text-xl font-medium">{name}</h3>
+        <span className="border-t mt-3 py-2 text-xs opacity-50 border-gray-400">
+          {categoryName}
+        </span>
+      </div>
+    </Link>
   );
 };
