13b3cd180ac1f85988af77f0935ea2c983047542
MoltenPL85 <bugaev.maxim.85@gmail.com>
2021-03-08 16:19:25 +0500

    Order subscription and take order

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,5 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import GoogleMapReact, { MapOptions, Maps, Position } from 'google-map-react';
+import GoogleMapReact from 'google-map-react';
+import { gql, useMutation, useSubscription } from '@apollo/client';
+import { FULL_ORDER_FRAGMENT } from '../../fragments';
+import { coockedOrders } from '../../__generated__/coockedOrders';
+import { useHistory } from 'react-router-dom';
+import { takeOrder, takeOrderVariables } from '../../__generated__/takeOrder';
+
+const COOKED_ORDERS_SUBSCRIPTION = gql`
+  subscription coockedOrders {
+    cookedOrders {
+      ...FullOrderParts
+    }
+  }
+  ${FULL_ORDER_FRAGMENT}
+`;
+
+const TAKE_ORDER_MUTATION = gql`
+  mutation takeOrder($input: TakeOrderInput!) {
+    takeOrder(input: $input) {
+      ok
+      error
+    }
+  }
+`;
 
 interface ICoords {
   lat: number;
@@ -18,6 +41,21 @@ export const Dashboard = () => {
   const [driverCoords, setDriverCoords] = useState<ICoords>({ lat: 0, lng: 0 });
   const [map, setMap] = useState<google.maps.Map>();
   const [maps, setMaps] = useState<any>();
+  const { data: coockedOrdersData } = useSubscription<coockedOrders>(
+    COOKED_ORDERS_SUBSCRIPTION
+  );
+  const history = useHistory();
+  const onCompleted = (data: takeOrder) => {
+    if (data.takeOrder.ok) {
+      history.push(`/orders/${coockedOrdersData?.cookedOrders.id}`);
+    }
+  };
+  const [takeOrderMutation] = useMutation<takeOrder, takeOrderVariables>(
+    TAKE_ORDER_MUTATION,
+    {
+      onCompleted,
+    }
+  );
   const onSuccess = ({
     coords: { latitude, longitude },
   }: GeolocationPosition) => {
@@ -46,6 +84,11 @@ export const Dashboard = () => {
       // );
     }
   }, [driverCoords.lat, driverCoords.lng]);
+  useEffect(() => {
+    if (coockedOrdersData?.cookedOrders.id) {
+      makeRoute();
+    }
+  }, [coockedOrdersData]);
 
   const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
     map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
@@ -54,7 +97,7 @@ export const Dashboard = () => {
   };
 
   // this should work on billing account
-  const onGetRouteClick = () => {
+  const makeRoute = () => {
     if (map) {
       const directionsService = new google.maps.DirectionsService();
       const directionsRenderer = new google.maps.DirectionsRenderer({
@@ -88,6 +131,16 @@ export const Dashboard = () => {
     }
   };
 
+  const triggerMutation = (orderId: number) => {
+    takeOrderMutation({
+      variables: {
+        input: {
+          id: orderId,
+        },
+      },
+    });
+  };
+
   return (
     <div>
       <div
@@ -105,7 +158,27 @@ export const Dashboard = () => {
           bootstrapURLKeys={{ key: 'AIzaSyAvA1iZ8EFblYdI1UzoYJdKbyvquSRP6D8' }}
         ></GoogleMapReact>
       </div>
-      <button onClick={onGetRouteClick}>Get route</button>
+      <div className="max-w-screen-sm mx-auto bg-white relative -top-10 shadow-lg py-8 px-5">
+        {coockedOrdersData?.cookedOrders.restaurant ? (
+          <>
+            <h1 className="text-center text-3xl font-medium">
+              New Coocked Order
+            </h1>
+            <h4 className="text-center my-3 text-2xl font-medium">
+              Pick it up soon @{' '}
+              {coockedOrdersData.cookedOrders.restaurant?.name}
+            </h4>
+            <button
+              onClick={() => triggerMutation(coockedOrdersData.cookedOrders.id)}
+              className="btn w-full block text-center mt-5"
+            >
+              Accept Challenge &rarr;
+            </button>
+          </>
+        ) : (
+          <h1 className="text-center text-3xl font-medium">No orders yet...</h1>
+        )}
+      </div>
     </div>
   );
 };
